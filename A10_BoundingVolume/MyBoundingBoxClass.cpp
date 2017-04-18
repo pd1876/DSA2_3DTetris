bdad81236e121a994e330c50bfe46c5ca016430c
MAIN\epn8794 <epn8794@rit.edu>
2017-04-17 22:44:50 -0400

    Memeingful commit, got the outer box volume working

--- A10_BoundingVolume/MyBoundingBoxClass.cpp
+++ A10_BoundingVolume/MyBoundingBoxClass.cpp
@@ -77,45 +77,68 @@ void MyBoundingBoxClass::RenderVolumeBox(std::vector<vector3> vertexList) {
 	if (m_bColliding)
 		v3Color = REYELLOW;
 
-	for (int i = 0; i < vertexList.size(); i++)
+	vector3 p1 = vector3(m_v3Min);
+	vector3 p2 = vector3(m_v3Min.x, m_v3Min.y, m_v3Max.z);
+	vector3 p3 = vector3(m_v3Max.x,m_v3Min.y,m_v3Min.z);
+	vector3 p4 = vector3(m_v3Max.x, m_v3Min.y, m_v3Max.z);
+	vector3 p5 = vector3(m_v3Min.x,m_v3Max.y,m_v3Max.z);
+	vector3 p6 = vector3(m_v3Min.x, m_v3Max.y, m_v3Min.z);
+	vector3 p7 = vector3(m_v3Max.x,m_v3Max.y,m_v3Min.z);
+	vector3 p8 = vector3(m_v3Max);
+	//vector3 diag = m_v3MaxG - m_v3MinG;
+	std::vector<vector3> volumeList;
+	volumeList.push_back(p1);
+	volumeList.push_back(p2);
+	volumeList.push_back(p3);
+	volumeList.push_back(p4);
+	volumeList.push_back(p5);
+	volumeList.push_back(p6);
+	volumeList.push_back(p7);
+	volumeList.push_back(p8);
+	for (int i = 0; i < 8; i++) {
+		volumeList[i] = vector3(m_m4ToWorld * vector4(volumeList[i], 1.0f));
+	}
+	
+	vector3 localMinCopy = volumeList[0];
+	vector3 localMaxCopy = volumeList[0];
+
+	for (int i = 1; i < volumeList.size(); i++)
 	{
-		if (m_v3MinG.x > vertexList[i].x)
+		if (localMinCopy.x > volumeList[i].x)
 		{
-			m_v3MinG.x = vertexList[i].x;
+			localMinCopy.x = volumeList[i].x;
 		}
-		else if (m_v3MaxG.x < vertexList[i].x)
+		else if (localMaxCopy.x < volumeList[i].x)
 		{
-			m_v3MaxG.x = vertexList[i].x;
-		}
-
-
-
-		if (m_v3MinG.y > vertexList[i].y)
-		{
-			m_v3MinG.y = vertexList[i].y;
-		}
-		else if (m_v3MaxG.y < vertexList[i].y)
-		{
-			m_v3MaxG.y = vertexList[i].y;
+			localMaxCopy.x = volumeList[i].x;
 		}
 
 
 
-		if (m_v3MinG.z > vertexList[i].z)
+		if (localMinCopy.y > volumeList[i].y)
 		{
-			m_v3MinG.z = vertexList[i].z;
+			localMinCopy.y = volumeList[i].y;
 		}
-		else if (m_v3MaxG.z < vertexList[i].z)
+		else if (localMaxCopy.y < volumeList[i].y)
 		{
-			m_v3MaxG.z = vertexList[i].z;
+			localMaxCopy.y = volumeList[i].y;
+		}
+
+
+
+		if (localMinCopy.z > volumeList[i].z)
+		{
+			localMinCopy.z = volumeList[i].z;
+		}
+		else if (localMaxCopy.z < volumeList[i].z)
+		{
+			localMaxCopy.z = volumeList[i].z;
 		}
 	}
-
-	vector3 diag = m_v3MaxG - m_v3MinG;
-
+	m_v3SizeG = localMaxCopy - localMinCopy;
 	m_pMeshMngr->AddCubeToRenderList(
 		glm::translate(m_v3CenterGlobal) * 
-		glm::scale(diag),
+		glm::scale(m_v3SizeG),
 		v3Color, WIRE);
 
 	std::cout << vertexList[1].x;
